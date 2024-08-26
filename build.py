1685bef5d56d8bc65a2c442d9134884a7d744fd7
ayaka <ayaka@soulik.info>
2024-08-26 18:49:52 +0800

    build: accept arguments for target and machine

--- build.py
+++ build.py
@@ -2,12 +2,15 @@
 
 import argparse
 import io
+import git
+import shutil
 import subprocess
 
 from git import Repo
+from pathlib import Path
 from tarfile import TarFile
 
-def check_build_directory(sub_mod):
+def check_build_directory(sub_mod: git.objects.submodule.base.Submodule):
     mod_name = sub_mod.name
     build_dir = 'build-{0}'.format(mod_name)
 
@@ -24,14 +27,48 @@ def prepare_submodule():
 
     [check_build_directory(sm) for sm in sms]
 
+def copy_quilt_files_to_build(sub_mod: git.objects.submodule.base.Submodule):
+    mod_name = sub_mod.name
+    build_dir = Path('build-{0}'.format(mod_name))
+    dst_dir = build_dir / 'patches'
+    patch_dir = '{0}-patches'.format(mod_name)
+    shutil.copytree(patch_dir, dst_dir)
+
+def copy_and_apply_patches():
+    repo = Repo()
+    sms = repo.submodules
+
+    [copy_quilt_files_to_build(sm) for sm in sms]
 
 if __name__ == '__main__':
+    targets = ['all', 'atf', 'u-boot']
+    machines = [ \
+            {'name': 'jdcloud-re-cp-03', 'description': 'JD Cloud Bali AX6000',
+             'soc': 'MT7986', 'ram': 'DDR4', 'sotarge': 'EMMC',
+             'dts': 'mt7986a-jdcloud_re-cp-03'},
+            ]
+
+    machines_choices = [d['name'] for d in machines]
+
     parser = argparse.ArgumentParser(prog='build.py')
-    parser.add_argument('step', choices=['unpack', 'patch', 'config', \
-                                          'build', 'firmware'])
+    subparsers = parser.add_subparsers(dest='step')
+
+    subparsers.add_parser('unpack', help='prepare the build directory')
+    subparsers.add_parser('patch', help='patch source codes')
+
+    parser_config = subparsers.add_parser('config')
+    parser_config.add_argument('machine', choices=machines_choices)
+    parser_config.add_argument('--ram', dest='boot_to_ram', action='store_true')
+
+    parser_build = subparsers.add_parser('build')
+    parser_build.add_argument('target', choices=targets)
+
+    subparsers.add_parser('firmware', help='packing the firmware')
 
     args = parser.parse_args()
 
     match args.step:
         case 'unpack':
             prepare_submodule()
+        case 'patch':
+            copy_and_apply_patches()
